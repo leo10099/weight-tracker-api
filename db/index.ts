7ec5ad7496a4eae64d27ce2211470a08579c3e23
leo10099 <leandrohanc@gmail.com>
2020-12-07 14:48:51 -0300

    Catch and log errors in Db connection

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,21 @@
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
+
+// Logger
+import Logger from 'services/Logger';
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
-  const client = await MongoClient.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+export const connectDatabase = async (): Promise<Db> => {
+  try {
+    const client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  const connection = await client.db(process.env.DB_NAME);
-
-  return connection;
+    const connection = await client.db(process.env.DB_NAME);
+    return connection;
+  } catch (e) {
+    Logger.error(e);
+    throw new Error(e);
+  }
 };
